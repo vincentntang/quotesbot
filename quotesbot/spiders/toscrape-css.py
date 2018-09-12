1ea274c0e692d7275810ed1e486167db42bd38ef
ACpython <10290348+Kagerjay@users.noreply.github.com>
2018-09-12 15:55:23 -0400

    Annotations of code

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -8,15 +8,16 @@ class ToScrapeCSSSpider(scrapy.Spider):
         'http://quotes.toscrape.com/',
     ]
 
+    # text, author, tags are all class names, respectively col 1,2,3 in spreadsheet
     def parse(self, response):
         for quote in response.css("div.quote"):
             yield {
                 'text': quote.css("span.text::text").extract_first(),
                 'author': quote.css("small.author::text").extract_first(),
-                'tags': quote.css("div.tags > a.tag::text").extract()
+                'tags': quote.css("div.tags > a.tag::text").extract() # there's multiple tags, denormalized in one csv cell
             }
 
-        next_page_url = response.css("li.next > a::attr(href)").extract_first()
+        next_page_url = response.css("li.next > a::attr(href)").extract_first() # checks for additional pages
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
 
